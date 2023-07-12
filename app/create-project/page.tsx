162a7e3f195dcb781df79305531e93135faf59bf
mdamapong <damapongm@gmail.com>
2023-07-12 11:29:58 -0700

    createform, upload

---
+++ app/create-project/page.tsx
@@ -0,0 +1,26 @@
+/** @format */
+
+import { redirect } from "next/navigation";
+
+import { getCurrentUser } from "@/lib/session";
+import Modal from "@/components/Model";
+import ProjectForm from "@/components/ProjectForm";
+
+const CreateProject = async () => {
+	const session = await getCurrentUser();
+
+	if (!session?.user) redirect("/");
+
+	return (
+		<Modal>
+			<h3 className="modal-head-text">Create a New Project</h3>
+
+			<ProjectForm
+				type="create"
+				session={session}
+			/>
+		</Modal>
+	);
+};
+
+export default CreateProject;
